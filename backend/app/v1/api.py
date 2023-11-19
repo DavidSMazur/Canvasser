46684c0b80992b1114ed1716792a37840c1822b2
Ri-Hong <riri.hong@gmail.com>
2023-11-19 02:42:55 -0500

    Mongo vectors works

--- backend/app/v1/api.py
+++ backend/app/v1/api.py
@@ -7,4 +7,4 @@ router = APIRouter()
 
 router.include_router(update.router, tags=["update"], prefix="/update")
 router.include_router(read_root.router, tags=["root"], prefix="/read_root")
-router.include_router(read_root.router, tags=["root"], prefix="/query")
+router.include_router(query.router, tags=["query"], prefix="/query")
--- backend/app/v1/functions/vectorizeMongo.py
+++ backend/app/v1/functions/vectorizeMongo.py
@@ -3,9 +3,8 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from langchain.vectorstores import MongoDBAtlasVectorSearch
 from langchain.document_loaders import PyMuPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-import numpy as np
 import certifi
 
 import dotenv
@@ -20,28 +19,32 @@ def get_db():
     db = client.MHacks
     return db
 
-loader = PyMuPDFLoader("Quasixenon.pdf")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=512, chunk_overlap=10)
-docs = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings()
+def vectorize_and_store():
+    loader = PyMuPDFLoader("Quasixenon.pdf")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=10)
+    docs = text_splitter.split_documents(documents)
+    embeddings = OpenAIEmbeddings()
 
 
-db = get_db()
-client = db.client  # Get the MongoClient object
-# collection_name = "embeddings"
-collection_name = "embeddings"
+    db = get_db()
+    client = db.client  # Get the MongoClient object
+    # collection_name = "embeddings"
+    collection_name = "embeddings"
 
-collection = db[collection_name]
-index_name = "default"
+    collection = db[collection_name]
+    index_name = "default"
 
-vectordb = MongoDBAtlasVectorSearch.from_documents(
-    docs, 
-    embeddings,
-    collection=collection,
-    index_name=index_name
-)
+    # print(docs)
+    vectordb = MongoDBAtlasVectorSearch.from_documents(
+        docs, 
+        embeddings,
+        collection=collection,
+        index_name=index_name
+    )
 
-query = "What is a quasixenon?"
-docs = vectordb.similarity_search(query)
-print(docs[0].page_content)
+    query = "What is a quasixenon?"
+    docs = vectordb.similarity_search(query)
+    # print(docs[0].page_content)
+
+vectorize_and_store()
